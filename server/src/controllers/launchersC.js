6b9a728e7c064d90b3b1036f676d89751076a967
Ariel Naiman <a0527633600@gmail.com>
2026-03-16 13:00:51 +0200

    break

--- server/src/controllers/launchersC.js
+++ server/src/controllers/launchersC.js
@@ -1,7 +1,18 @@
-export function getLaunchers(req, res) {
+import { getLaunchersS } from "../services/launchersS"
+
+export function getLaunchersC(req, res) {
     try {
-        const launchers = getLaunchers()
+        const launchers = getLaunchersS()
         return res.status(200).json(launchers)
+    } catch (error) {
+        console.error(error.message)
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+export function createLauncherS(req, res) {
+    try {
+        const { name, rocketType, latitude, longitude, city } = req.body
     } catch (error) {
         console.error(error.message)
         return res.status(500).json({ error: error.message })
--- server/src/services/launchersS.js
+++ server/src/services/launchersS.js
@@ -2,10 +2,19 @@ import { db } from "../db/connection.js";
 
 const collection = db.collection('users');
 
-export function getLaunchers() {
+export function getLaunchersS() {
     try {
         const launchers = collection.find({})
         return launchers
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+export async function createLauncherS(launcher) {
+    try {
+        const result = await collection.insertOne(launcher);
+        console.log(result);
     } catch (error) {
         console.error(error.message)
     }
