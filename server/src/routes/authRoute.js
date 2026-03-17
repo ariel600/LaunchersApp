cb53fe59af3f30ce08a3f21e16e39621c3554f3a
Ariel Naiman <a0527633600@gmail.com>
2026-03-17 12:02:45 +0200

    Base login

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createUserController, deleteUserController, updateUserController } from "../controllers/authController.js"
+import { createUserController, deleteUserController, loginController, updateUserController } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post("/register", createUserController)
 router.put("/register/update", updateUserController)
 router.delete("/register/delete/:id", deleteUserController)
+router.post("/login", loginController)
 
 export default router
